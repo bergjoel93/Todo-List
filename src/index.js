0b70ba1b85b1e900ed255cd83068e6c3339004e3
Joel Berg <bergjoel93@gmail.com>
2024-04-03 11:33:26 -0600

    This is a larger commit because I'm now utilizing a web dev environment that I build using a tutorial.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//import generateJoke from "./generateJoke.js"
+import './styles/style.css'
+import laughing from './assets/laughing.svg'
+
+
+
+//
+// const laughImg = document.getElementById('laughImg');
+// laughImg.src = laughing;
+// console.log(generateJoke());
+
+// const jokeBtn = document.getElementById('jokeBtn');
+// jokeBtn.addEventListener('click', generateJoke);
+
+// generateJoke();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,56 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //added
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [ //added
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: 'body',
-      scriptLoading: 'defer',
-    })
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-
-
-};
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname,'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true, 
+        hot: true, 
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+}
